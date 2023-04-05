ddfd5b493b7d07aaea3554d6d88f0584ca00cf03
TheEmotionalProgrammer <isidorotamassia@gmail.com>
2023-04-05 10:24:16 +0200

    In realtà non è cambiato nulla ma penso sia buggato e mi dice di committare ancora modified:   register.php modified:   script.js

--- register.php
+++ register.php
@@ -10,16 +10,15 @@
     $username = $_POST["username"];
     $fullname = $_POST["fullname"];
     $number = $_POST["number"];
-    $gender = 'male'; //da cambiare
     $wins = 0;
     
-    echo "Your data: username = $username, fullname = $fullname, email = $email, number = $number, gender = $gender <br>";
+    echo "Your data: username = $username, fullname = $fullname, email = $email, number = $number <br>";
     echo '<input type = "button" value = "Torna alla home" onClick = "history.go(-2); return true" name = "button">';
     $dbconnection = pg_connect("host = localhost dbname = dama user = postgres password = kub3tt0SQL") or die('Could not connect');
     //per qualche motivo mi dice che le funzioni non esistono ANCHE SE l'highlighting le consiglia e funzionano
 
     //Inserisco il nuovo utente nel db con zero vittorie
-    $query ="INSERT INTO utente VALUES('$username', '$fullname', '$email', '$number', '$password','$gender', 0)";
+    $query ="INSERT INTO utente VALUES('$username', '$fullname', '$email', '$number', '$password', 0)";
     $result = pg_query($dbconnection, $query) or die('la query non va');
     pg_free_result($result);
     pg_close($dbconnection);
